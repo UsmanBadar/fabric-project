377dd28fe419b2136249feca7751f114318966b7
Usman <usman.badar006@gmail.com>
2025-12-02 17:09:56 +1100

    added new functions to fabric core

--- config/fabric_core/__init__.py
+++ config/fabric_core/__init__.py
@@ -3,10 +3,15 @@ Fabric Core - Reusable modules for Microsoft Fabric CLI operations.
 """
 
 from .utils import *
+from .login import login
+from .load_config import load_config_from_file
 
 __all__ = [
+    "load_local_env_file",
     "set_stdout_encoding_to_utf_8",
     "get_fab_cli_executable_path",
     "run_fabric_cli_command",
-    "call_azure_fabric_rest_api"
+    "call_azure_fabric_rest_api",
+    "login",
+    "load_config_from_file"
 ]
