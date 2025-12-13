afe539d42401c355efa76ee953e94c613880a2c2
Usman <usman.badar006@gmail.com>
2025-12-13 20:45:02 +1100

    updated __init__ file

--- config/fabric_core/__init__.py
+++ config/fabric_core/__init__.py
@@ -5,7 +5,11 @@ Fabric Core - Reusable modules for Microsoft Fabric CLI operations.
 from .utils import *
 from .login import login
 from .load_config import load_config_from_file
-from .capacities import check_capacity_exists, get_capacity_status, wait_for_capacity_ready, create_capacity, suspend_capacity
+from .capacities import (check_capacity_exists, get_capacity_status, 
+                         wait_for_capacity_ready, create_capacity, suspend_capacity)
+from .workspaces import (get_workspace_id, workspace_exists, create_workspace, assign_permissions)
+
+from .git_integration import (get_or_create_git_connection, update_workspace_from_git, connect_workspace_to_git)
 
 __all__ = [
     "load_local_env_file",
@@ -19,5 +23,12 @@ __all__ = [
     "get_capacity_status",
     "wait_for_capacity_ready",
     "create_capacity", 
-    "suspend_capacity"
+    "suspend_capacity",
+    "get_workspace_id", 
+    "workspace_exists", 
+    "create_workspace", 
+    "assign_permissions",
+    "get_or_create_git_connection", 
+    "update_workspace_from_git", 
+    "connect_workspace_to_git"
 ]
