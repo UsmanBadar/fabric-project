0978d5c8471471f93a2351c242dcb6e3f34c2d83
Usman <usman.badar006@gmail.com>
2025-12-13 20:44:47 +1100

    initial script for creating infra

--- config/scripts/deploy_infra_from_yaml_template.py
+++ config/scripts/deploy_infra_from_yaml_template.py
@@ -4,7 +4,8 @@ ROOT_DIR = Path(__file__).resolve().parents[2]
 print(ROOT_DIR)
 sys.path.append(str(ROOT_DIR))
 
-from config.fabric_core import login, load_config_from_file, create_capacity
+from config.fabric_core import (login, load_config_from_file, create_capacity, create_workspace,
+                                assign_permissions, get_or_create_git_connection, connect_workspace_to_git)
 
 
 
@@ -25,13 +26,34 @@ def main():
         print("===config file loaded===")
     else:
         raise RuntimeError("Config file not found.")
-    
+
+    print("===== Creating Capacities =====")
+
     capacities_config = config["capacities"]
     capacities_defaults_config = config["azure"]["capacity_defaults"]
     resource_group = capacities_defaults_config["resource_group"]
+
     
     for item in capacities_config:
         create_capacity(item, resource_group, capacities_defaults_config)
+
+    print("===== Creating Workspaces and setting up Git connection =====")
+
+    workspaces_config = config["workspaces"]
+    security_groups = config["azure"]["security_groups"]
+    github_config = config["github"]
+
+
+    for item in workspaces_config:
+        print(item)
+        print(item["permissions"])
+        workspace_id = create_workspace(item)
+        assign_permissions(workspace_id = workspace_id, permissions = item["permissions"], security_groups = security_groups)
+        git_connection_id = get_or_create_git_connection(github_config)
+        if git_connection_id:
+            connect_workspace_to_git(workspace_id = workspace_id, workspace_name = item["name"], 
+                                     directory_name = item["connect_to_git_folder"], git_config = github_config, connection_id = git_connection_id)
+
 
 
 
